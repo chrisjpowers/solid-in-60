37e4abd7db010f942d9c743d2605ddc59dafa928
Chris Powers <chrisjpowers@gmail.com>
2018-06-07 08:08:46 -0500

    Finished first stab at LSP

--- 03-lsp-0.js
+++ 03-lsp-0.js
@@ -96,3 +96,54 @@ class UserAuthController {
     }
   }
 }
+
+//--------------------
+
+class UserAuthController {
+  login(user, pw) {
+    let login = this.getLoginForUser(user);
+    let rejectionPath = login.getRejectionPath();
+    if (rejectionPath) return {redirect: rejectionPath};
+
+    if (login.authenicateAsync) {
+      login.authenticateAsync(pw, (err) => {
+        !err ?
+          {redirect: login.successPath} :
+          {redirect: "/login"};
+      });
+    } else {
+      return login.authenticate(pw) ?
+        {redirect: login.successPath} :
+        {redirect: "/login"};
+    }
+  }
+}
+
+class Under18Login {
+  constructor(user) {
+    this.successPath = "/home";
+    this.user = user;
+  }
+  authenticate(pw) {
+    return UserAuth.isAuthenticatedBy(this.user.username, pw);
+  }
+  getRejectionPath() {
+    return this.pastCurfew() ? "/curfew" : null;
+  }
+  pastCurfew(date) {
+    const hours = date.getHours();
+    return hours < 7 || hours > 22;
+  }
+}
+
+class FacebookLogin {
+  constructor(user) {
+    this.successPath = '/home';
+    this.user = user;
+  }
+  // Asynchronous!
+  authenticateAsync(pw, cb) {
+    UserAuth.authenticateWithFacebook(this.user.fb, pw, cb);
+  }
+  getRejectionPath() { return null; }
+}
