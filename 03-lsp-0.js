bd8caa364edb751bae5f17608c5d37cd517a2623
Chris Powers <chrisjpowers@gmail.com>
2018-06-06 21:57:33 -0500

    Started LSP

---
+++ 03-lsp-0.js
@@ -0,0 +1,98 @@
+class UserAuthController {
+  login(user, pw) {
+    let login = this.getLoginForUser(user);
+    return login.authenticate(pw) ?
+      {redirect: login.successPath} :
+      {redirect: "/login"};
+  }
+}
+
+class FacebookLogin {
+  constructor(user) {
+    this.successPath = '/home';
+    this.user = user;
+  }
+  // Asynchronous!
+  authenticate(pw, cb) {
+    UserAuth.authenticateWithFacebook(this.user.fb, pw, cb);
+  }
+}
+
+//----------------
+
+class UserAuthController {
+  login(user, pw) {
+    let login = this.getLoginForUser(user);
+    if (login instanceof FacebookLogin) {
+      login.authenticate(pw, (err) => {
+        err ?
+          {redirect: "/login"} :
+          {redirect: login.successPath};
+      });
+    } else {
+      return login.authenticate(pw) ?
+        {redirect: login.successPath} :
+        {redirect: "/login"};
+    }
+  }
+}
+
+class FacebookLogin {
+  constructor(user) {
+    this.successPath = '/home';
+    this.user = user;
+  }
+  // Asynchronous!
+  authenticate(pw, cb) {
+    UserAuth.authenticateWithFacebook(this.user.fb, pw, cb);
+  }
+}
+
+//----------------
+
+class UserAuthController {
+  login(user, pw) {
+    let login = this.getLoginForUser(user);
+    if (login instanceof Under18Login) {
+      if (login.pastCurfew(new Date())) {
+        return {redirect: "/curfew"};
+      }
+    }
+    if (login instanceof FacebookLogin) {
+      login.authenticate(pw, (err) => {
+        !err ?
+          {redirect: login.successPath} :
+          {redirect: "/login"};
+      });
+    } else {
+      return login.authenticate(pw) ?
+        {redirect: login.successPath} :
+        {redirect: "/login"};
+    }
+  }
+}
+
+class Under18Login {
+  constructor(user) {
+    this.successPath = "/home";
+    this.user = user;
+  }
+  authenticate(pw) {
+    return UserAuth.isAuthenticatedBy(this.user.username, pw);
+  }
+  pastCurfew(date) {
+    const hours = date.getHours();
+    return hours < 7 || hours > 22;
+  }
+}
+
+class FacebookLogin {
+  constructor(user) {
+    this.successPath = '/home';
+    this.user = user;
+  }
+  // Asynchronous!
+  authenticate(pw, cb) {
+    UserAuth.authenticateWithFacebook(this.user.fb, pw, cb);
+  }
+}
