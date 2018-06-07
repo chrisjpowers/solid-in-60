ad175119765e121b300a1722d97b18b48efcd5d7
Chris Powers <chrisjpowers@gmail.com>
2018-06-06 21:22:43 -0500

    Added OCP

---
+++ 02-ocp-0.js
@@ -0,0 +1,85 @@
+class UserAuthController {
+  login(user, pw) {
+    let result, path;
+    if (user.type === "admin") {
+      path = "/admin";
+      result = UserAuth.authenticateWithLdap(user.username, pw);
+    } else if (user.freeTrial === true) {
+      path = "/upsell";
+      result = user.trialKey === pw;
+    } else {
+      path = "/home";
+      result = UserAuth.isAuthenticatedBy(user.username, pw);
+    }
+    return result ? {redirect: path} : {redirect: "/login"};
+  }
+}
+
+//---------------------
+
+class UserAuthController {
+  login(user, pw) {
+    let login, result, page;
+    if (user.type === "admin") {
+      login = new AdminLogin(user);
+    } else if (user.freeTrial === true) {
+      login = new FreeTrialLogin(user)
+    } else {
+      login = new MemberLogin(user)
+    }
+    return login.authenticate(pw) ?
+      {redirect: login.successPath} :
+      {redirect: "/login"};
+  }
+}
+
+class AdminLogin {
+  constructor(user) {
+    this.successPath = "/admin";
+    this.user = user;
+  }
+  authenticate(pw) {
+    return UserAuth.authenticateWithLdap(this.user.username, pw);
+  }
+}
+
+class FreeTrialLogin {
+  constructor(user) {
+    this.successPath = "/upsell";
+    this.user = user;
+  }
+  authenticate(pw) {
+    return this.user.trialKey === pw;
+  }
+}
+
+class MemberLogin {
+  constructor(user) {
+    this.successPath = "/home";
+    this.user = user;
+  }
+  authenticate(pw) {
+    return UserAuth.isAuthenticatedBy(this.user.username, pw);
+  }
+}
+
+//------------------
+
+class UserAuthController {
+  login(user, pw) {
+    let login = this.getLoginForUser(user);
+    return login.authenticate(pw) ?
+      {redirect: login.successPath} :
+      {redirect: "/login"};
+  }
+
+  getLoginForUser(user) {
+    if (user.type === "admin") {
+      return new AdminLogin(user);
+    } else if (user.freeTrial === true) {
+      return new FreeTrialLogin(user);
+    } else {
+      return new MemberLogin(user);
+    }
+  }
+}
